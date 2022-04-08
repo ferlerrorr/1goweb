30a8141686873e49a461149a904f8aa8e9f7cec1
Ferl Jhon D. Javier <80177329+ferlerrorr@users.noreply.github.com>
2022-04-08 11:19:01 +0800

    In1gotech.com
    Missing Page Solutions Update

--- wp-content/themes/wp-in1go/404.php
+++ wp-content/themes/wp-in1go/404.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
    <div id="content">
      <h2>Error 404 - Not Found</h2>
-     <img alt="404_image_in1go" style="width: 99vw; margin-top: 3.4%; z-index: 0;" src="https://in1gotech.com/wp-content/uploads/2021/11/IN1GO-ERROR-PAGE-1-WIDE-V2.png">
+     <!-- For Live -->
+     <!-- <img alt="404_image_in1go" style="width: 99vw; margin-top: 3.4%; z-index: 0;" src="https://in1gotech.com/wp-content/uploads/2021/11/IN1GO-ERROR-PAGE-1-WIDE-V2.png"> -->
+    <!-- For Local And Staging -->
+     <img alt="404_image_in1go" style="width: 99vw; margin-top: 3.4%; z-index: 0;" src="https://ferlerrorr.github.io/in1gotech/wp-content/uploads/2021/11/IN1GO-ERROR-PAGE-1-WIDE-V2.png">
    </div>
 <?php get_footer(); ?>
